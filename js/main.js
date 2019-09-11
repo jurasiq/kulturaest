ba8e7035b67f82e1f990b5f4f0765759f3f46f70
user <user@sslntwrk.lan>
2019-09-11 12:15:57 +0300

    add header slider to page

--- js/main.js
+++ js/main.js
@@ -9,6 +9,15 @@ $(document).ready(function () {
 		else if ($(document).scrollTop() > 0) { show_small_logo(); }
 	});
 
+	$('#slider-header').slideshow({
+        randomize: false,      // Randomize the play order of the slides.
+        slideDuration: 6000,  // Duration of each induvidual slide.
+        fadeDuration: 1000,    // Duration of the fading transition. Should be shorter than slideDuration.
+        animate: true,        // Turn css animations on or off.
+        pauseOnTabBlur: true, // Pause the slideshow when the tab is out of focus. This prevents glitches with setTimeout().
+        enableLog: false      // Enable log messages to the console. Useful for debugging.
+    });
+
 	$("#slider-news").owlCarousel({
 		center: true,
 		items: 4,
