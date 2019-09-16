0e4d829ed9ade5b1e8cf7455449aa75738fe4e39
Yuriy <juras-iq@yandex.ru>
2019-09-16 23:59:53 +0300

    Form correct

--- js/main.js
+++ js/main.js
@@ -101,3 +101,37 @@ $(window).on('load', function () {
 	check_scroll();
 	$('.backdrop').delay(1000).fadeOut('slow');
 });
+
+'use strict';
+
+;
+(function ($, window, document, undefined) {
+	$('.inputfile').each(function () {
+		var $input = $(this),
+			$label = $input.next('label'),
+			labelVal = $label.html();
+
+		$input.on('change', function (e) {
+			var fileName = '';
+
+			if (this.files && this.files.length > 1)
+				fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+			else if (e.target.value)
+				fileName = e.target.value.split('\\').pop();
+
+			if (fileName)
+				$label.find('span').html(fileName);
+			else
+				$label.html(labelVal);
+		});
+
+		// Firefox bug fix
+		$input
+			.on('focus', function () {
+				$input.addClass('has-focus');
+			})
+			.on('blur', function () {
+				$input.removeClass('has-focus');
+			});
+	});
+})(jQuery, window, document);
