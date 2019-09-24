991a1923cb1887de90a1370834e4c6dc94188de2
user <user@sslntwrk.lan>
2019-09-24 19:10:33 +0300

    add map to contacts

---
+++ js/map.js
@@ -0,0 +1,55 @@
+ymaps.ready(function () {
+	var addresses = {};
+	$.getJSON("js/map_data.json", function(json) {
+		addresses = json['addresses'];
+		var mapPoints = [];
+		addresses.forEach(function(addr){
+			var htmlAddress = "<ul class='address' id='"+addr["id"]+"'>";
+			htmlAddress += "<li><p><span><strong>М</strong></span>"+addr["metro"]+"</p></li>";
+			htmlAddress += "<li><p><span class='mapmarker'></span>"+addr["address"]+"</p></li>";
+			htmlAddress += "<li><p><span class='phone'></span>"+addr["phone"]+"</p></li>";
+			htmlAddress += "<li><p><span class='clock'></span>"+addr["clock"]+"</p></li>";
+			htmlAddress += "<li><p><span>Формат:</span><strong>"+addr["format"]+"</strong></p></li>";
+			$("#addresses").append(htmlAddress);
+			mapPoints[addr["id"]] = new ymaps.Placemark(addr['coordinates'], {iconContent: addr["id"]}, {
+															iconLayout: 'default#image',
+															iconImageHref: 'img/small-logo.png',
+															iconImageSize: [48, 48],
+															iconImageOffset: [-24, -24],
+															
+														});
+		});
+		
+		var myMap = new ymaps.Map('map', {
+				center: [59.930934, 30.361884],
+				zoom: 11
+			}, {
+				searchControlProvider: 'yandex#search'
+		});
+
+		
+		for (var key in mapPoints) {
+			myMap.geoObjects.add(mapPoints[key]);
+		};
+		
+
+		// events for clicking on marks
+		for (key in mapPoints) {
+			//alert(mapPoints[key].properties.get("iconContent"));
+			mapPoints[key].events.add('click', function (e) { 
+				var elemId = e.get('target').properties.get("iconContent");
+				$('.address').removeClass('address-selected');
+				$('#'+elemId).addClass('address-selected');
+			});
+		}
+
+		for (key in addresses) {
+			var addr = addresses[key];
+			$('#'+addr["id"]).click({coords:addr["coordinates"]}, function(e){
+				$('.address').removeClass('address-selected');
+				$(this).addClass('address-selected');
+				myMap.setCenter(e.data.coords, 15, {duration: 1000});		
+			});
+		}
+	});
+});
